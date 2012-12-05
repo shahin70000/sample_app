df3f94df34323b8f8e23d598930bf21c3bc76020
shahin70000 <shahin70000@gmail.com>
2012-12-05 12:06:39 +0300

    helper method full_title added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "StaticPages" do
 			page.should have_selector('h1', :text => 'Sample App')
 		end	  
 
-		it "should have the right title" do
-		  visit '/static_pages/home'
-		  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-		end
+		it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "Home")
+    end
 	end
 
 
@@ -24,7 +30,8 @@ describe "StaticPages" do
  		end
 		it "should have the right title" do
 		  visit '/static_pages/help'
-		  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+		  page.should have_selector('title',
+		  					 :text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -38,7 +45,8 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 		  visit '/static_pages/about'
-		  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+		  page.should have_selector('title',
+		  			 :text => "Ruby on Rails Tutorial Sample App | About Us")
 		end		
 	  
 	end
