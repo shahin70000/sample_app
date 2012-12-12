1b4ddc92a6e1d2beb6fb58f360b8263cdf7c7828
shahin70000 <shahin70000@gmail.com>
2012-12-12 17:35:41 +0300

    Micropost model generated, migrated and associated with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   has_secure_password
+	has_many :microposts
 
   validates_confirmation_of :password
   validates :password_confirmation, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,8 @@
 require 'spec_helper'
 
 describe User do
-  before {@user = User.new(name: "Example User", email: "user@example.com", password: "Foobar", password_confirmation: "Foobar")}
+  before {@user = User.new(name: "Example User", email: "user@example.com", 
+                          password: "Foobar", password_confirmation: "Foobar")}
 
   subject { @user }
 
@@ -25,6 +26,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -114,5 +116,19 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+  
+  describe "microposts associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
   end
 end
