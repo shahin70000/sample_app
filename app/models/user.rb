65dc042fd076fa1d40c828a4e3efa98b107be9f2
shahin70000 <shahin70000@gmail.com>
2012-12-16 11:12:04 +0300

    Sample following data

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,12 @@ class User < ActiveRecord::Base
 
   has_secure_password
 	has_many :microposts, dependent: :destroy
+
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
 
+	has_many :reverse_relationships, class_name: "Relationship", :foreign_key => "followed_id", dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
 
   validates_confirmation_of :password
   validates :password_confirmation, presence: true
@@ -43,7 +46,7 @@ class User < ActiveRecord::Base
 	end
 
 	def unfollow!(other_user)
-		relationships.find_by_followed_id(followed_id: other_user.id).destroy
+		relationships.find_by_followed_id(other_user.id).destroy
 	end
 
 	private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,8 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }  
   it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }  
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
@@ -165,7 +167,12 @@ describe User do
     end
 
     it { should be_following(other_user) }
-    its(:followed_user) { should include(other_user)}
+    its(:followed_users) { should include(other_user)}
+
+    describe "followed users" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
 
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }      
@@ -173,5 +180,7 @@ describe User do
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
+
+
   end
 end
